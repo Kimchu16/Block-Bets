01e9e145f5979a400fa2aa280c7e8107bd35f966
Shirolicht <68757902+Kimchu16@users.noreply.github.com>
2026-02-02 20:03:26 +0000

    Fixed slot machine voxel shape for a more accurate collision shape

--- src/main/java/com/kimchu16/blockbets/block/custom/SlotMachineBlock.java
+++ src/main/java/com/kimchu16/blockbets/block/custom/SlotMachineBlock.java
@@ -4,15 +4,25 @@ import com.mojang.serialization.MapCodec;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalFacingBlock;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 import org.jetbrains.annotations.Nullable;
 
 public class SlotMachineBlock extends HorizontalFacingBlock {
     public static final MapCodec<SlotMachineBlock> CODEC = createCodec(SlotMachineBlock::new);
+    private static final VoxelShape SHAPE = Block.createCuboidShape(2.0, 0.0, 1.0, 15.0, 16.0, 15.0);
 
     public SlotMachineBlock(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
     }
 
     @Override
