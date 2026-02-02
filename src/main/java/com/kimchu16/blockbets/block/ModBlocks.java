d4a1eb2da9724aa985dd9ddf4abc5de431fcdd29
Shirolicht <68757902+Kimchu16@users.noreply.github.com>
2026-02-02 13:03:42 +0000

    Added slot machine block into creative inventory

--- src/main/java/com/kimchu16/blockbets/block/ModBlocks.java
+++ src/main/java/com/kimchu16/blockbets/block/ModBlocks.java
@@ -1,14 +1,25 @@
 package com.kimchu16.blockbets.block;
 
 import com.kimchu16.blockbets.BlockBets;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
+    public static final Block SLOT_MACHINE_BLOCK = registerBlock("slot_machine_block",
+            new Block(AbstractBlock.Settings.create()
+                    .strength(4.0f, 5.0f)
+                    .requiresTool()
+                    .sounds(BlockSoundGroup.METAL)
+            )
+    );
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
@@ -22,5 +33,9 @@ public class ModBlocks {
 
     public static void registerModBlocks() {
         BlockBets.LOGGER.info("Registering Mod Blocks for " + BlockBets.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(entries -> {
+            entries.add(ModBlocks.SLOT_MACHINE_BLOCK);
+        });
     }
 }
